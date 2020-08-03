96c47f9d2cf9e2351a79f11327e7bb619da38755
Mitesh Modi <mitesh.modi003@gmail.com>
2020-08-02 22:47:16 -0400

    updates

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+    "imagebaseurl":"https://miteshmodi003.github.io/miteshmodi/",
+    "name": "Mitesh Modi",
+    "role": "Senior Software Engineer",
+    "linkedinId":"https://www.linkedin.com/in/miteshkmodi/",
+    "roleDescription": "I like dabbling in various parts of frontend development and love to learn new technologies and skills.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/miteshkmodi/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Miteshmodi003",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"Certified Full Stack Web Developer from Georgia Tech Professional Education with over three years of experience in IT Sales Team at GEICO. Experienced software engineer in different areas such as Production Support, Release Management, and Greenfield projects. Well-versed at identifying/gathering business requirements/challenges and making enhancements to our current sales applications that drives our internal as well as external customers. Successfully worked on projects that resulted in decreased numbers of production bugs and prevented non-production bugs. Absolutely excited to develop and support robust and responsive web applications with no defects and well-designed and documented code that is easily readable to drive business KPIs with a combined team effort. Proficient in MERN stack web applications as well as .NET MVC Application Development with SQL/NOSQL Databases.",
+    "address":"Atlanta, GA",
+    "website":"https://miteshmodi003.github.io/miteshmodi/",
+    "education":[
+      {
+        "UniversityName":"Georgia Gwinnett College",
+        "specialization":"Bachelor's of Science - Information Technology",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2016",
+        "Achievements":"Summa Cum Laude"
+      },
+      {
+        "UniversityName":"Georgia Tech",
+        "specialization":"Full Stack Flex Web Developer",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Certificate of Completion"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"GEICO",
+        "specialization":"Full Stack Software Developer",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2017",
+        "Achievements":"GEICO Certified .NET Developer Level III"
+      }
+    ],
+    "skillsDescription":"I am a full stack web developer specializing in...",
+    "skills":[
+      {
+        "skillname":"nodejs"
+      },
+      {
+        "skillname":"javascript"
+      },
+      {
+        "skillname":"reactjs"
+      },
+      {
+        "skillname":"mongodb"
+      },
+      {
+        "skillname":"sql"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
